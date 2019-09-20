f5b9994c2f2d1ea7b34ec0b1c43e071384ee6483
chakio <y-kawasaki@keio.jp>
2019-09-20 20:59:13 +0900

    add ssh_aliases

--- install_alias.sh
+++ install_alias.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 echo "source $HOME/dotfiles/.aliases">> $HOME/.bashrc
 echo "source $HOME/dotfiles/.ros_aliases">> $HOME/.bashrc
+echo "source $HOME/dotfiles/.ssh_aliases">> $HOME/.bashrc
 source $HOME/.bashrc
