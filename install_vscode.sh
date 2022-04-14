c0cde2a35510c5b27da49a063d27bd9294f0557e
chakio <y-kawasaki@keio.jp>
2022-04-14 13:55:44 +0900

    :hammer: FIX: delete symblic link setting

--- install_vscode.sh
+++ install_vscode.sh
@@ -4,21 +4,3 @@ sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode s
 sudo apt install apt-transport-https
 sudo apt update
 sudo apt-get install code
-
-
-#エディタの設定を共有
-
-if [ -d ~/.config/Code/ ]; then
-	rm  -rf ~/.config/Code/User/
-else
-	mkdir ~/.config/Code/
-fi
-ln -fs ~/dotfiles/vscode/User/ ~/.config/Code/
-#プラグインを共有
-if [ -d ~/.vscode/ ]; then
-	rm  -rf ~/.vscode/extentions/
-else
-	mkdir ~/.vscode/
-fi
-
-ln -fs ~/dotfiles/vscode/extensions/ ~/.vscode/
