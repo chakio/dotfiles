bf35db6e6018df7f39ce5e7dac0ad1cfae158233
chakio <y-kawasaki@keio.jp>
2025-02-15 21:19:47 +0900

    FIX: install tp link wifi

--- install_tp_link_wifi_adaptor.sh
+++ install_tp_link_wifi_adaptor.sh
@@ -1,4 +1,9 @@
 sudo apt-get update && sudo apt-get install -y dkms build-essential linux-headers-generic
-git clone https://github.com/aircrack-ng/rtl8812au.git
-cd rtl8812au && sudo make dkms_install
+# delete the folder if it already exists
+sudo rm -rf rtl8812au
+git clone https://github.com/aircrack-ng/rtl8812au.git 
+cd rtl8812au 
+git checkout 63cf0b4
+sudo make dkms_remove
+sudo make dkms_install
 sudo rm -rf rtl8812au
